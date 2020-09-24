29e00f9ba08c2f62a32070c3e81eb62b4613e80a
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-09-24 10:16:18 +0100

    Update main

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 80;
 
 app.get("/", (request, response) => {
   response.send("Hello, World!");
 });
-
-app.set("port", process.env.PORT);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port.toString()} `);
