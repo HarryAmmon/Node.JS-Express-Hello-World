15de7860571327b2c33f4124bae212c7f8f38498
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-09-24 10:56:16 +0100

    Add comments

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 80;
+const express = require("express"); // import express
+const app = express(); // create an instance of express
+const port = process.env.PORT || 80; // create a port object, process.env.PORT will be used by Azure
 
 app.get("/", (request, response) => {
   response.send("Hello, World!");
-});
+}); // Create a GET response
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port.toString()} `);
-});
+}); // Register app on port
